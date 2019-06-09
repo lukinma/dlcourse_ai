1ce818067cd07840176f321e52c5867aedae0c50
lukinma <lukinma@gmail.com>
2019-06-09 23:53:38 +0300

    1d gradient_check

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -32,9 +32,14 @@ def check_gradient(f, x, delta=1e-5, tol=1e-4):
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
+        # numeric_grad_at_ix = 0
 
         # TODO compute value of numeric gradient of f to idx
+        print('x: ' + str(x[ix]) + ' delta: ' + str(delta) + ' x+d: ' + str(x[ix] + delta))
+        print('it: ' + str(it))
+        print('f(x): ' + str(f(x[ix])))
+        numeric_grad_at_ix = (f(x[ix] + delta)[0] - f(x[ix] - delta)[0]) / (2*delta)
+
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
             ix, analytic_grad_at_ix, numeric_grad_at_ix))
