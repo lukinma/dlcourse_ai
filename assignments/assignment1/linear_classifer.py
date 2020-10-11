softmax fix

assignments/assignment1/linear_classifer.py
@@ -15,6 +15,8 @@ def softmax(predictions):
     '''
     # TODO implement softmax
     # Your final implementation shouldn't have any loops
+
+    print(predictions.shape)
     predictions -= np.max(predictions)
     ei = np.exp(predictions)
     denominator = np.sum(ei)
@@ -38,9 +40,17 @@ def cross_entropy_loss(probs, target_index):
     '''
     # TODO implement cross-entropy
     # Your final implementation shouldn't have any loops
-    true_vals = np.zeros(len(probs))
-    true_vals[target_index] = 1
+    true_vals = np.zeros(probs.shape)
+    # print('true_vals: ', true_vals, 'shape: ', true_vals.shape)
+    # print('probs', probs)
+    # print('target_index', target_index, 'shape', target_index.shape)
+    # print("t ", true_vals[(0, 0)], true_vals[(0, 1)])
+    for i in range(len(target_index)):
+        true_vals[(i, target_index)] = 1
+    # true_vals[target_index] = 1
     res = np.sum(true_vals*np.log(probs))
+    print('true_vals:', true_vals)
+    print('cross_entropy_loss: ', res)
     return res
     
     
@@ -80,9 +90,23 @@ def softmax_with_cross_entropy(predictions, target_index):
     # Your final implementation shouldn't have any loops
     
     #predictions is an array of 'y'
+    single = (predictions.ndim == 1)
+    if single:
+        predictions = predictions.reshape(1, predictions.shape[0])
+        target_index = np.array([target_index])
     
-    sf = softmax(predictions)
-    loss = cross_entropy_loss(sf, target_index)
+    sf = softmax(predictions)                       # S
+    # print('predictions: ', predictions)
+    # print('softmax: ', sf)
+    # print('target_index: ', target_index)
+    loss = cross_entropy_loss(sf, target_index)     # L
+
+    indicator = np.zeros(sf.shape)
+    indicator[np.arange(sf.shape[0]), target_index] = 1     # 1(y)
+    dprediction = (sf - indicator) / predictions.shape[0]   # dL/dZ = (S - 1(y)) / N
+
+    if single:
+        dprediction = dprediction.reshape(dprediction.size)
     
     return loss, dprediction
 